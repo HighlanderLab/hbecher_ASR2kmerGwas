179b115e840350a31d95cc0251f533d2c6f4f9b5
Hannes Becher <hannesbecher@gmx.de>
2025-10-03 15:29:29 +0100

    better comments

--- 02makeReads.sh
+++ 02makeReads.sh
@@ -13,6 +13,12 @@ gs=10000 # genome size (haploid)
 for f in `ls $inDir/ind*.fasta`; do
 
     base=$(basename $f .fasta)
+    # -e is the base error rate
+    # -d is the avg. outer distance between the two ends
+    # -s is the stddev of the outer distance
+    # -1 and -2 are the read lengths
+    # -N is the number of read pairs to simulate
+    echo "Simulating reads for $base"
     $wgsim -r 0 -R 0 -e 0.0001 -d 550 -s 50  \
           -1 $rl -2 $rl -N $((gs/2/$rl*$dep)) \
           $f haploGenomes/${base}_fw.fq  haploGenomes/${base}_rw.fq    > /dev/null
--- 03runKmc.sh
+++ 03runKmc.sh
@@ -1,5 +1,5 @@
 
-# make sure the k-mer toolkit kc is in path
+# make sure the k-mer toolkit kmc is in path
 # can be installed via anacond
 source /home/hbecher/miniconda3/etc/profile.d/conda.sh
 conda activate kmc
@@ -13,7 +13,7 @@ for i in `ls $inDir/ind*.fasta`; do
     printf "$inDir/$base"_fw".fq\n$inDir/$base"_rw".fq" > $inDir"/readfiles_"$base
     echo "Counting k-mers for $base"
     kmc -k21 -t3 -cs500000000 -fq -ci1 -fq @$inDir/readfiles_$base $inDir/${base}_db21 $inDir
-    echo "Generating spectrum dump for $base"
+    echo "Generating spectrum for $base"
     kmc_tools transform $inDir/${base}_db21 histogram $inDir/${base}_hist21.txt
     #rm $inDir/readfiles_$base
 done
